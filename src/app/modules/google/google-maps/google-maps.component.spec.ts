1eabef64b9699f6026d84a95d1cccd57249c7a39
BeguinMattis <mattisbeguin.etude@gmail.com>
2019-03-09 22:04:08 +0100

    Fix unit tests

--- src/app/modules/google/google-maps/google-maps.component.spec.ts
+++ src/app/modules/google/google-maps/google-maps.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { GoogleMapsComponent } from './google-maps.component';
 
@@ -8,7 +9,10 @@ describe('GoogleMapsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GoogleMapsComponent ]
+      declarations: [ GoogleMapsComponent ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     })
     .compileComponents();
   }));
