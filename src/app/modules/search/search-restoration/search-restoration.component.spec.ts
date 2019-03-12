61ad8402add3fe921e67cae9726cecfff8f602d2
BeguinMattis <mattisbeguin.etude@gmail.com>
2019-03-12 21:10:50 +0100

    Add HTML5 Geolocation API in the search-restoration component

--- src/app/modules/search/search-restoration/search-restoration.component.spec.ts
+++ src/app/modules/search/search-restoration/search-restoration.component.spec.ts
@@ -1,8 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchRestorationComponent } from './search-restoration.component';
 
-describe('LocateUserComponent', () => {
+describe('SearchRestorationComponent', () => {
   let component: SearchRestorationComponent;
   let fixture: ComponentFixture<SearchRestorationComponent>;
 
--- src/app/modules/search/search-restoration/search-restoration.component.ts
+++ src/app/modules/search/search-restoration/search-restoration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marker } from '../../../models/marker.model';
 
 @Component({
   selector: 'app-search-restoration',
@@ -10,6 +11,45 @@ export class SearchRestorationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getUserLocation(): void {
+    this.HTML5GeolocationAPI();
+  }
+
+  HTML5GeolocationAPI(): boolean {
+    if ((window.navigator) && (window.navigator.geolocation)) {
+      window.navigator.geolocation.getCurrentPosition((position: Position) => {
+          const marker: Marker = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          };
+          console.log('Marker : ' + JSON.stringify(marker));
+          return true;
+        },
+        (error: PositionError) => {
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              console.error('User denied the request for geolocation!');
+              break;
+            case error.POSITION_UNAVAILABLE:
+              console.error('Location information is unavailable!');
+              break;
+            case error.TIMEOUT:
+              console.error('The request to get user location timed out!');
+              break;
+            default:
+              console.error('An unknown error occurred!');
+              break;
+          }
+          console.error(error.message);
+          return false;
+        },
+        {enableHighAccuracy: true});
+    } else {
+      console.error('Geolocation is not supported by this browser!');
+      return false;
+    }
   }
 
 }
