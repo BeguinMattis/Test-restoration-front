1742aae30c093cf7772e1f7b4dd7c072091365c2
BeguinMattis <mattisbeguin.etude@gmail.com>
2019-03-17 17:20:22 +0100

    Implement Marker Service and add unit tests in the google-maps module

--- src/app/modules/google-maps/google-maps/google-maps.component.spec.ts
+++ src/app/modules/google-maps/google-maps/google-maps.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { GoogleMapsComponent } from './google-maps.component';
+import { Marker } from '../../../models/marker.model';
 
 describe('GoogleMapsComponent', () => {
   let component: GoogleMapsComponent;
@@ -22,7 +23,51 @@ describe('GoogleMapsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+    it('Should create the component', () => {
+      const parisMarker = {
+        latitude: 48.8534,
+        longitude: 2.3488,
+        display: false
+      };
+      spyOn(component['mapService'], 'setUserMarker');
+      component.ngOnInit();
+      expect(component.restorationMarkers).toEqual([]);
+      expect(component['mapService'].setUserMarker).toHaveBeenCalledWith(parisMarker);
+      expect(component.userMarker).toEqual(parisMarker);
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('addRestorationMarker', () => {
+    it('Should add a marker in the restorationMarkers array', () => {
+      const restorationMarker: Marker = {
+        latitude: 0,
+        longitude: 0,
+        display: true
+      };
+      component.restorationMarkers = [];
+      component.addRestorationMarker(restorationMarker);
+      expect(component.restorationMarkers.length).toEqual(1);
+      expect(component.restorationMarkers).toEqual([restorationMarker]);
+    });
+
+    it('Should not add a marker in the restorationMarkers array', () => {
+      const restorationMarker: any = null;
+      component.restorationMarkers = [];
+      component.addRestorationMarker(restorationMarker);
+      expect(component.restorationMarkers.length).toEqual(0);
+      expect(component.restorationMarkers).toEqual([]);
+    });
+  });
+
+  describe('ngOnDestroy', () => {
+    it('Should destroy the component', () => {
+      spyOn(component['_ngUnsubscribe'], 'next');
+      spyOn(component['_ngUnsubscribe'], 'complete');
+      component.ngOnDestroy();
+      expect(component['_ngUnsubscribe'].next).toHaveBeenCalled();
+      expect(component['_ngUnsubscribe'].complete).toHaveBeenCalled();
+    });
   });
 });
--- src/app/modules/google-maps/google-maps/google-maps.component.ts
+++ src/app/modules/google-maps/google-maps/google-maps.component.ts
@@ -1,38 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Marker } from '../../../models/marker.model';
-import { Subscription } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { Subject, Subscription } from 'rxjs';
 import { MapService } from '../../../services/map/map.service';
+import { MarkerService } from '../../../services/marker/marker.service';
+import { takeUntil, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-google-maps',
   templateUrl: './google-maps.component.html',
   styleUrls: ['./google-maps.component.css']
 })
-export class GoogleMapsComponent implements OnInit {
+export class GoogleMapsComponent implements OnInit, OnDestroy {
 
   userMarker: Marker;
   restorationMarkers: Marker[];
+  private _ngUnsubscribe: Subject<any>;
   userMarkerSubscription: Subscription;
 
   constructor(private mapService: MapService) { }
 
   ngOnInit() {
-    this.userMarker = {
+    this.restorationMarkers = [];
+    this._ngUnsubscribe = new Subject<any>();
+    this.userMarkerSubscription = this.mapService.userMarkerSubject
+      .pipe(takeUntil(this._ngUnsubscribe))
+      .pipe(filter((userMarker: Marker) => MarkerService.check(userMarker) === true))
+      .subscribe((userMarker: Marker) => {
+        this.userMarker = userMarker;
+      }
+    );
+    const parisMarker: Marker = {
       latitude: 48.8534,
       longitude: 2.3488,
       display: false
     };
-    this.userMarkerSubscription = this.mapService.userMarkerSubject
-      .pipe(filter((marker) => !!marker && !!marker.latitude && !!marker.longitude))
-      .subscribe((marker: Marker) => {
-        this.userMarker = marker;
-      }
-    );
+    this.mapService.setUserMarker(parisMarker);
   }
 
-  addRestorationMarker(marker: Marker): void {
-    this.restorationMarkers.push(marker);
+  addRestorationMarker(restorationMarker: Marker): void {
+    if (MarkerService.check(restorationMarker) === true) {
+      this.restorationMarkers.push(restorationMarker);
+    }
   }
 
+  ngOnDestroy() {
+    this._ngUnsubscribe.next();
+    this._ngUnsubscribe.complete();
+  }
 }
