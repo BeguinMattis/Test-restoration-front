5304c86d7db564b8b44c592619af318b43bfbf4f
BeguinMattis <mattisbeguin.etude@gmail.com>
2019-04-16 21:03:00 +0200

    Test to fix deployment error on Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 export const environment = {
   production: true,
+  test_restoration: {
+    back_api_base_url: 'https://test-restoration-back.herokuapp.com',
+    authentication_google_resource_path: '/authentication/google'
+  },
   api_key: 'AIzaSyB8jOSKqZ5Bre5C5zHgVedJgV8LLz-6w24',
-  client_id: 'client-id',
-  google_geolocation_api_url: 'https://www.googleapis.com/geolocation/v1/geolocate'
+  client_id: '78163528702-v3nnjkp8dbr7k43i90pd5bb4e5uatv1h.apps.googleusercontent.com',
+  google_geolocation_api_base_url_resource_path: 'https://www.googleapis.com/geolocation/v1/geolocate'
 };
