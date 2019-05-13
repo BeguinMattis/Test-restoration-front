7b668f6e77b6fdc6f929008e9e3f378671fae33a
BeguinMattis <mattisbeguin.etude@gmail.com>
2019-05-13 22:07:48 +0200

    Fix error during construction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,9 @@ export const environment = {
   production: false,
   test_restoration: {
     back_api_base_url: 'https://test-restoration-back.herokuapp.com',
-    authentication_google_resource_path: '/authentication/google'
+    authentication_google_resource_path: '/authentication/google',
+    opinion_list: '/review/list',
+    opinion_add: '/review/add'
   },
   api_key: 'AIzaSyB8jOSKqZ5Bre5C5zHgVedJgV8LLz-6w24',
   client_id: '78163528702-v3nnjkp8dbr7k43i90pd5bb4e5uatv1h.apps.googleusercontent.com',
