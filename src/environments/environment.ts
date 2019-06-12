2c696e24eb731355eace325a18d7b04dd81e4842
BeguinMattis <mattisbeguin.etude@gmail.com>
2019-06-13 00:39:01 +0200

    Add CSS styles for the main component layout

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   test_restoration: {
     back_api_base_url: 'https://test-restoration-back.herokuapp.com',
     authentication_google_resource_path: '/authentication/google',
-    opinion_list: '/review/list',
-    opinion_add: '/review/add'
+    opinion_list: '/opinion/list',
+    opinion_add: '/opinion/add'
   },
   api_key: 'AIzaSyB8jOSKqZ5Bre5C5zHgVedJgV8LLz-6w24',
   client_id: '78163528702-v3nnjkp8dbr7k43i90pd5bb4e5uatv1h.apps.googleusercontent.com',
